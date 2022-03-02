f0f3d7a838cafcafb81b7848d7875aaee44d7d44
Patrick <jpmnyc@mac.com>
2022-03-01 21:49:44 -0500

    simplify

---
+++ static/js/table.js
@@ -0,0 +1,17 @@
+function addRowHandlers() {
+  var table = document.getElementById("recipe");
+  var rows = table.getElementsByTagName("tr");
+  for (i = 0; i < rows.length; i++) {
+    var currentRow = table.rows[i];
+    var createClickHandler = function(row) {
+      return function() {
+        var cell = row.getElementsByTagName("td")[0];
+        var id = cell.innerHTML;
+        alert("id:" + id);
+      };
+    };
+    currentRow.onclick = createClickHandler(currentRow);
+  }
+}
+
+window.onload = addRowHandlers();
